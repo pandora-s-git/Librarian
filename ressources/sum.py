215e49c852386ba9e87174d39ec15b8aaaac0764
pandora <128635000+pandora-s-git@users.noreply.github.com>
2024-03-01 18:39:21 +0100

    Add files via upload

--- ressources/sum.py
+++ ressources/sum.py
@@ -1,25 +1,29 @@
 print("[ SUM ] Loading SUM Requirements...")
 
-from transformers import RobertaTokenizerFast, EncoderDecoderModel
+## from transformers import RobertaTokenizerFast, EncoderDecoderModel  # in case u wanna use a french one
+from transformers import pipeline
 
 class SUM():
     def __init__(self):
 
         print("[ SUM ] Loading model for summarization...")
 
-        ## ckpt = 'mrm8488/camembert2camembert_shared-finetuned-french-summarization' in case u wanna use a french one
+        ## ckpt = 'mrm8488/camembert2camembert_shared-finetuned-french-summarization'
+        ## tokenizer = RobertaTokenizerFast.from_pretrained(ckpt)
+        ## self.tokenizer = tokenizer
+        ## self.model = EncoderDecoderModel.from_pretrained(ckpt).to("cpu")
+
         ckpt = "facebook/bart-large-cnn"
-        tokenizer = RobertaTokenizerFast.from_pretrained(ckpt)
-        model = EncoderDecoderModel.from_pretrained(ckpt).to("cpu")
-        self.tokenizer = tokenizer
-        self.model = model 
+        self.model = pipeline("summarization", model = ckpt)
 
     def _generate_summary(self, text):
-        inputs = self.tokenizer([text], padding="max_length", truncation=True, max_length=512, return_tensors="pt")
-        input_ids = inputs.input_ids.to("cpu")
-        attention_mask = inputs.attention_mask.to("cpu")
-        output = self.model.generate(input_ids, attention_mask=attention_mask)
-        return self.tokenizer.decode(output[0], skip_special_tokens=True)
+        ## inputs = self.tokenizer([text], padding="max_length", truncation=True, max_length=512, return_tensors="pt")
+        ## input_ids = inputs.input_ids.to("cpu")
+        ## attention_mask = inputs.attention_mask.to("cpu")
+        ## output = self.model.generate(input_ids, attention_mask=attention_mask)
+        ## return self.tokenizer.decode(output[0], skip_special_tokens=True)
+
+        return "".join(list(self.model(text, max_length=256, min_length=32, do_sample=False)[0]["summary_text"]))
 
     def sum(self, txt: str):
 
